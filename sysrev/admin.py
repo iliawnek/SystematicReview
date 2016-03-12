8743a61972975172e328f9ff7ec350875cfad401
Ken Li <iliawnek@gmail.com>
2016-03-12 19:21:59 +0000

    Register models in admin console

--- sysrev/admin.py
+++ sysrev/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from sysrev.models import Researcher, Review, Paper
 
-# Register your models here.
+admin.register(Researcher)
+admin.register(Review)
+admin.register(Paper)
