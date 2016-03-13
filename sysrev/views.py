8e1c7d9371a38b21a95fc876718b7a1c2d9a4169
Ken Li <iliawnek@gmail.com>
2016-03-13 17:09:12 +0000

    django-registration-redux login page now works

--- sysrev/views.py
+++ sysrev/views.py
@@ -3,6 +3,6 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
 
-# @login_required
+@login_required
 def index(request):
     return render(request, 'sysrev/index.html')
