4a5ee177b15a211210eef8516d6f336b318f34b9
Ken Li <iliawnek@gmail.com>
2016-03-17 16:26:12 +0000

    Create card-based layout for index page

--- sysrev/views.py
+++ sysrev/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 
+from sysrev.models import Review
+
 
 @login_required
 def index(request):
-    return render(request, 'sysrev/index.html')
+    context_dict = {"reviews": Review.objects.filter(user=request.user)}
+    return render(request, 'sysrev/index.html', context_dict)
 
 
 @login_required
