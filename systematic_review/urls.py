870fdd3f47085d8604363008441017ac8401f3df
Ewan Morton <ewan.morton@outlook.com>
2016-03-17 18:56:50 +0000

    Added password reset template

--- systematic_review/urls.py
+++ systematic_review/urls.py
@@ -14,4 +14,5 @@ urlpatterns = patterns('',
                        url(r'^admin/', include(admin.site.urls)),
                        url(r'^accounts/register/$', MyRegistrationView.as_view(), name='registration_register'),
                        url(r'^accounts/', include('registration.backends.simple.urls')),
+                       url(r'^accounts/', include('django.contrib.auth.urls')),
                        )
