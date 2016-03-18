08102289f96c1c8cf793c67529c0f7eb6d6da5a3
David Robertson <david@davidr.me>
2016-03-18 03:19:53 +0000

    Add DB migration

---
+++ sysrev/migrations/0001_initial.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+from django.conf import settings
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Paper',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(max_length=128)),
+                ('authors', models.CharField(max_length=128)),
+                ('abstract', models.TextField(default=b'')),
+                ('publish_date', models.DateField(null=True)),
+                ('url', models.URLField(default=b'')),
+                ('notes', models.TextField(default=b'')),
+                ('pool', models.CharField(default=b'A', max_length=1, choices=[(b'A', b'Abstract pool'), (b'D', b'Document pool'), (b'F', b'Final pool'), (b'R', b'Rejected')])),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.CreateModel(
+            name='Review',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('title', models.CharField(unique=True, max_length=128)),
+                ('slug', models.SlugField()),
+                ('description', models.TextField(default=b'')),
+                ('date_created', models.DateTimeField(auto_now_add=True)),
+                ('last_modified', models.DateTimeField(auto_now=True)),
+                ('completed', models.BooleanField(default=False)),
+                ('query', models.TextField(default=b'')),
+                ('abstract_pool_size', models.IntegerField(default=0)),
+                ('document_pool_size', models.IntegerField(default=0)),
+                ('final_pool_size', models.IntegerField(default=0)),
+                ('rejected_pool_size', models.IntegerField(default=0)),
+                ('user', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+        migrations.AddField(
+            model_name='paper',
+            name='review',
+            field=models.ForeignKey(to='sysrev.Review'),
+            preserve_default=True,
+        ),
+    ]
