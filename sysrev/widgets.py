b429b36c06da68d51bc6af473da8c38f2569a7c3
Ross Allan <ross@nallar.me>
2016-03-19 18:56:00 +0000

    Add querywidget.js and include it for QueryWidget

--- sysrev/widgets.py
+++ sysrev/widgets.py
@@ -1,8 +1,17 @@
-
 from django.forms import widgets
 from django.utils.safestring import *
 
+# See static/js/querywidget.js
 class QueryWidget(widgets.Textarea):
+    def __init__(self, attrs=None):
+        default_attrs = {'class': "queryWidget"}
+        if attrs:
+            default_attrs.update(attrs)
+        super(QueryWidget, self).__init__(default_attrs)
+
+    def script(self):
+        return mark_safe("<script src='/static/js/querywidget.js' type='text/javascript' defer></script>")
+
     def render(self, name, value, attrs=None):
         textAreaHtml = super(QueryWidget, self).render(name, value, attrs)
-        return mark_safe("I am the query editor widget, with some custom behaviour.") + textAreaHtml
+        return self.script() + textAreaHtml
