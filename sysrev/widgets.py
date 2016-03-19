760a952851664232b11ad95acb8884dda59308a4
Ross Allan <ross@nallar.me>
2016-03-19 18:59:46 +0000

    Add <noscript> message to indicate that only the advanced query editor is available.

--- sysrev/widgets.py
+++ sysrev/widgets.py
@@ -10,7 +10,9 @@ class QueryWidget(widgets.Textarea):
         super(QueryWidget, self).__init__(default_attrs)
 
     def script(self):
-        return mark_safe("<script src='/static/js/querywidget.js' type='text/javascript' defer></script>")
+        return mark_safe("<noscript>Please enable javascript to use the query editor. Without it enabled, you can only "
+                         "use the advanced editor</noscript>"
+                         "<script src='/static/js/querywidget.js' type='text/javascript' defer></script>")
 
     def render(self, name, value, attrs=None):
         textAreaHtml = super(QueryWidget, self).render(name, value, attrs)
