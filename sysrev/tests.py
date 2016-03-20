a0ab8efe3578ba333907baecb95dcc1b044fb702
Ross Allan <ross@nallar.me>
2016-03-20 16:53:38 +0000

    Add PubMed.paper and test for it

--- sysrev/tests.py
+++ sysrev/tests.py
@@ -1,7 +1,15 @@
 from django.test import TestCase
 from api import PubMed
 
+
 class PubmedQueryTestCase(TestCase):
     def test_query(self):
         result = PubMed.query("smoking")
         self.assertGreater(result[u'Count'], 25000, "Expected >25000 results for smoking")
+
+    def test_paper(self):
+        result = PubMed.paper([25929677])
+
+        self.assertEquals(len(result[0][u'MedlineCitation'][u'Article'][u'AuthorList']),
+                          7,
+                          "25929677 should have 7 authors")
