ad0b850b46ac6924e217570e7b7278beb1c14789
David Robertson <david@davidr.me>
2016-03-20 03:57:29 +0000

    'Reading aid' seems superfluous

--- static/js/jquery.js
+++ static/js/jquery.js
@@ -1,11 +1,3 @@
 $(function () {
   $('[data-toggle="tooltip"]').tooltip();
 });
-
-$(function () {
-  $("#reading").click(function(){
-    $(".panel-body").toggleClass("panel-reading");
-    $("#reading").toggleClass("active");
-  });
-  $('[data-toggle="tooltip"]').tooltip();
-});
