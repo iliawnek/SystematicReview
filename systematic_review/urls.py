72a64180be87adb3998cbe11efc6a9967a5571e1
David Robertson <david@davidr.me>
2016-03-20 03:53:04 +0000

    Cleanup

--- systematic_review/urls.py
+++ systematic_review/urls.py
@@ -1,23 +1,16 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
-from sysrev import views
-from sysrev.views import *
-from registration.backends.simple.views import RegistrationView
-
-
-class MyRegistrationView(RegistrationView):
-    def get_success_url(self, user=None):
-        return "/"
+from django.contrib   import admin
+from sysrev.views     import *
 
 urlpatterns = patterns(
     '',
-    url(r'^$',                          ReviewListView.as_view(),     name='index'),
-    url(r'^review/(?P<pk>\d+)(-([\w\-]+))?/$', ReviewDetailView.as_view(),   name='review'),
-    url(r'^review/(?P<pk>\d+)(-([\w\-]+))?/(?P<pk2>\d+)/$', PaperDetailView.as_view(),   name='paper'),
-    url(r'^create/',                    ReviewCreateWizard.as_view(), name='create'),
-    url(r'^profile/',                   ProfileView.as_view(),        name='profile'),
-    url(r'^admin/',                     include(admin.site.urls)),
-    url(r'^accounts/register/$',        MyRegistrationView.as_view(), name='registration_register'),
-    url(r'^accounts/',                  include('registration.backends.simple.urls')),
-    url(r'^accounts/',                  include('django.contrib.auth.urls')),
+    url(r'^$',                                              ReviewListView.as_view(),     name='index'),
+    url(r'^review/(?P<pk>\d+)(-([\w\-]+))?/$',              ReviewDetailView.as_view(),   name='review'),
+    url(r'^review/(?P<pk>\d+)(-([\w\-]+))?/(?P<pk2>\d+)/$', PaperDetailView.as_view(),    name='paper'),
+    url(r'^create/',                                        ReviewCreateWizard.as_view(), name='create'),
+    url(r'^profile/',                                       ProfileView.as_view(),        name='profile'),
+    url(r'^admin/',                                         include(admin.site.urls)),
+    url(r'^accounts/register/$',                            SRRegistrationView.as_view(), name='registration_register'),
+    url(r'^accounts/',                                      include('registration.backends.simple.urls')),
+    url(r'^accounts/',                                      include('django.contrib.auth.urls')),
 )
