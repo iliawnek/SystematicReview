2f7477dfbcbd322ab2f564ac395b98cb21bf25d0
Ken Li <iliawnek@gmail.com>
2016-03-21 13:33:21 +0000

    Collapse icon instead of "show/hide" text

--- static/js/sysrev.js
+++ static/js/sysrev.js
@@ -1,3 +1,9 @@
 $(function () {
-  $('[data-toggle="tooltip"]').tooltip();
+    $('[data-toggle="tooltip"]').tooltip();
+});
+
+$(function () {
+    $(".collapse-button").click(function () {
+        $(".collapse-button").toggleClass("glyphicon-menu-down glyphicon-menu-up")
+    })
 });
