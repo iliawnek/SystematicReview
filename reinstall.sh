93a97dab349517d35fafe33a98c09298aa501772
Ross Allan <ross@nallar.me>
2016-03-21 10:28:27 +0000

    Add `reinstall.sh` script which starts up a fresh install

---
+++ reinstall.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Installs pip requirements
+pip install -r requirements.txt
+
+# Remove any existing DB
+rm db.sqlite3
+
+# Make migrations and migrate
+python manage.py makemigrations
+python manage.py migrate
+
+# Populate fresh DB with test data
+python populate.py
+
+# Start local server
+python manage.py runserver
