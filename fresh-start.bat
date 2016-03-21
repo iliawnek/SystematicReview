bf2f0158bd594a1811be3b3028b394b211da3dd7
Ewan Morton <ewan.morton@outlook.com>
2016-03-21 15:48:05 +0000

    fresh-start.bat die if the database cannot be deleted

--- fresh-start.bat
+++ fresh-start.bat
@@ -1,4 +1,7 @@
 pip install -r requirements.txt
+@echo off
+( type nul >> db.sqlite3 ) 2>nul || (echo The database is currently in use by another process. && exit /b)
+@echo on
 del db.sqlite3
 python manage.py makemigrations
 python manage.py migrate
